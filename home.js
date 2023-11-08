d238374422bb11ba5ea9dc9b6209be4bb9f16f00
emmettzero <148921353+emmettzero@users.noreply.github.com>
2023-11-07 20:03:50 -0600

    party

--- home.js
+++ home.js
@@ -1,10 +1,46 @@
+var partying = false;
+
 function switchMode() {
     let page = document.querySelector("body");
-    if (page.style.backgroundColor === "black") {
+    if (page.style.backgroundColor === "rgb(38, 36, 36)") {
         page.style.backgroundColor = "white";
         page.style.color = "black";
     } else {
-        page.style.backgroundColor = "black";
+        page.style.backgroundColor = "rgb(38, 36, 36)";
+        page.style.color = "white";
+    }
+}
+
+function changeColors() {
+    let page = document.querySelector("body");
+	let pageColor = page.style.backgroundColor;
+    if (pageColor === "yellow") {
+    	page.style.backgroundColor="red";
+    }
+    else if (pageColor === "red") {
+    	page.style.backgroundColor="blue";
+    }
+    else if (pageColor === "blue") {
+    	page.style.backgroundColor="purple";
+    }
+    else if (pageColor === "purple") {
+    	page.style.backgroundColor="green";
+    }
+    else {
+    	page.style.backgroundColor="yellow";
+    }
+    
+}
+
+function partyMode() {
+    if (partying === false) {
+    	setInterval(changeColors, 100);
+        partying = true;
+    }
+    else {
+        let page = document.querySelector("body");
+    	partying = false;
+        page.style.backgroundColor = "rgb(38, 36, 36)";
         page.style.color = "white";
     }
 }
